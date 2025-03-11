b506d27b6c71e38eff0f8b6d06d73e11ee520381
talmid17 <bengaist10@gmail.com>
2025-03-11 14:48:50 +0200

    first commit

--- app/src/main/java/com/example/db_ben/NoteDAO.java
+++ app/src/main/java/com/example/db_ben/NoteDAO.java
@@ -1,8 +1,19 @@
 package com.example.db_ben;
 
-import androidx.room.Dao;
+import android.provider.ContactsContract;
 
-@Dao
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
 public interface NoteDAO {
-
+    @Insert
+    void insert(Note note);
+    @Update
+    void update(Note note);
+    @Delete
+    void delete(Note note);
+    @Query("SELECT * FROM notes WHERE id = :id")
+    Note getNoteByID(int id);
 }
